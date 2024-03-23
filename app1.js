1198e18ab7e0c173381e15f024b46ce353a5f0bd
Kishore4739 <121333142+Kishore4739@users.noreply.github.com>
2024-03-23 12:02:48 +0530

    Create app1.js

---
+++ app1.js
@@ -0,0 +1,10 @@
+import './App.css';
+import DataFetching from './DataFetching';
+function App() {
+  return (
+    <div className="App">
+      <DataFetching />
+    </div>
+  );
+}
+export default App;
